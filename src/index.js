2dc65db26a77627c6361c93b18a19af5506c9dd5
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-02-10 10:42:10 -0600

    Style page sidebar and main content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./normalize.css";
 import "./style.css";
 import createTodoCard from "./todo-card";
-/* Image by <a href="https://pixabay.com/users/gdj-1086657/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=7746445">Gordon Johnson</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=7746445">Pixabay</a> */
+/
 
