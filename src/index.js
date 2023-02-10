60fec54f21910abed23b70609e8dee655c454f9a
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-02-10 17:22:06 -0600

    Add toggle to form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./normalize.css";
 import "./style.css";
+import "./interface";
 /* import createTodoCard from "./todo-card"; */
--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,13 @@
-
-
-
-
-const createTaskPopup = () => {
-    
-}
-
-
-
-export {createTaskPopup}
+const formVisibleToggle = (() => {
+  const createButton = document.querySelector(".add-button");
+  const form = document.querySelector(".form");
+  createButton.addEventListener("click", () => {
+    if (form.classList.contains("visible")) {
+      form.classList.add("no-visible");
+      form.classList.remove("visible");
+    } else {
+      form.classList.add("visible");
+      form.classList.remove("no-visible");
+    }
+  });
+})();
